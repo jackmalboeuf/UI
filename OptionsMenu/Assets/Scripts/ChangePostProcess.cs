87b97a254cc202f20ddbdce7dceb758c4f93d0ff
jackmalboeuf <jack.malboeuf@loop.colum.edu>
2018-08-15 16:31:50 -0500

    8/15

---
+++ OptionsMenu/Assets/Scripts/ChangePostProcess.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class ChangePostProcess : MonoBehaviour
+{
+    PostProcessVolume volume;
+    AmbientOcclusion ambientOcclusionLayer;
+    Bloom bloomLayer;
+    ColorGrading colorGradingLayer;
+    DepthOfField depthOfFieldLayer;
+    Vignette vignetteLayer;
+
+    private void Start()
+    {
+        volume = GetComponent<PostProcessVolume>();
+
+        volume.profile.TryGetSettings(out ambientOcclusionLayer);
+        volume.profile.TryGetSettings(out bloomLayer);
+        volume.profile.TryGetSettings(out colorGradingLayer);
+        volume.profile.TryGetSettings(out depthOfFieldLayer);
+        volume.profile.TryGetSettings(out vignetteLayer);
+    }
+
+    public void AmbientOcclusionActive()
+    {
+        ambientOcclusionLayer.quality.value = AmbientOcclusionQuality.Ultra;
+        bloomLayer.active = true;
+    }
+}
